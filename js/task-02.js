087a4ce295bcebed6af7a02245651a5d385b680f
Alex Borets <alexborets2812@gmail.com>
2022-01-16 20:15:18 +0200

    task 2 rdy

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,14 @@
 const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listOfElem = document.querySelector('#ingredients');
+
+const listOfIngr = ingredients.map(ingr => {
+  const itemLi = document.createElement('li');
+  itemLi.textContent = ingr;
+  itemLi.classList.add('item');
+  console.log(itemLi);
+  return itemLi;
+});
+
+listOfElem.append(...listOfIngr);
+console.log(listOfIngr);
