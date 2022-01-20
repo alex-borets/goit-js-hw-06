574bce9d092b59d89dd20d7c0ceece149b1f8baf
Alex Borets <alexborets2812@gmail.com>
2022-01-20 12:59:09 +0200

    task 7 upd
    changed default fontSize for textOutput

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const rangeInput = document.querySelector('#font-size-control');
 const textOutput = document.querySelector('#text');
 
+textOutput.style.fontSize = `${Number(rangeInput.value)}px`;
+
 rangeInput.addEventListener('input', event => {
   textOutput.style.fontSize = `${Number(event.currentTarget.value)}px`;
 });
