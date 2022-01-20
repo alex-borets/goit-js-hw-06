592ea03a6e1dea1f011cdf16337fa15a6e9ae04a
Alex Borets <alexborets2812@gmail.com>
2022-01-20 13:59:06 +0200

    task 9 rdy

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const bodyElem = document.querySelector('body');
+const colorElem = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// function changeColorButtonClick() {
+//   bodyElem.style.backgroundColor = getRandomHexColor();
+//   colorElem.textContent = bodyElem.style.backgroundColor;
+// }
+
+changeColorButton.addEventListener('click', () => {
+  bodyElem.style.backgroundColor = getRandomHexColor();
+  colorElem.textContent = bodyElem.style.backgroundColor;
+});
