13ad8bb6c9401428d702fb24eb0a68f7e04f019d
Alex Borets <alexborets2812@gmail.com>
2022-01-20 13:14:20 +0200

    task 8 rdy

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
-event.preventDefault();
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', loginButtonClick);
+
+function loginButtonClick(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    alert('Please fill in all the fields!');
+  }
+
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
